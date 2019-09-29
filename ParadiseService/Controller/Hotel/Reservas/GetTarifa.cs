7583470d178a9f8ee5ac432fcbba59493a341177
Diego Rodriguez <diegorrod91@gmail.com>
2019-09-29 16:01:50 -0300

    Solucion a #10

---
+++ ParadiseService/Controller/Hotel/Reservas/GetTarifa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace Paradise.Service.Controller.Hotel.Reservas
+{
+    public partial class ReservasController : ApiController
+    {
+        [Route("{resNro}/tarifa")]
+        [HttpGet]
+        public async Task<IHttpActionResult> GetTarifa(string resNro)
+        {
+            return await Task.Run<IHttpActionResult>(() =>
+            {
+                try
+                {
+                    using (var db = new ParadiseDataContext())
+                    {
+                        db.CommandTimeout = SQL_TIMEOUTE;
+                        var result = (from tarifa
+                                     in db.RESOCUP
+                                      where tarifa.ResNro == Convert.ToInt32(resNro)
+                                      select tarifa).ToList();
+                        return Ok(result);
+                    }
+                }
+                catch (System.Data.SqlClient.SqlException)
+                {
+                    return ResponseMessage(new HttpResponseMessage(HttpStatusCode.GatewayTimeout));
+                }
+                catch (Exception ex)
+                {
+                    return InternalServerError(ex);
+                }
+            });
+        }
+    }
+}
