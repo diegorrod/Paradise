ec6e809f327fbbdc31d70ddf613de76a722ca5a1
Diego Rodriguez <30572826+diegorrod@users.noreply.github.com>
2019-09-29 12:32:11 -0300

    Update ReservaController #3
    Soluciona #3

--- ParadiseService/Controller/Hotel/Reservas/ReservasController.cs
+++ ParadiseService/Controller/Hotel/Reservas/ReservasController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -38,10 +40,29 @@ namespace Paradise.Service.Controller.Hotel.Reservas
                 }
             });
         }
-                                      where obs.ResNro == resNro
+
+        // Reserva observaciones mucamas
+        [Route("{resNro}/observaciones-mucamas")]
+        [HttpGet]
+        public async Task<IHttpActionResult> GetObservacionesMucamas(int resNro)
+        {
+            return await Task.Run<IHttpActionResult>(() =>
+            {
+                try
+                {
+                    using (var db = new ParadiseDataContext())
+                    {
+                        db.CommandTimeout = 5;
+                        var result = (from obs
+                                     in db.RESERVAMUCAMA
+                                      where obs.ResNro == Convert.ToInt32(resNro)
                                       select obs).ToList();
                         return Ok(result);
                     }
+                }
+                catch (System.Data.SqlClient.SqlException)
+                {
+                    return ResponseMessage(new HttpResponseMessage(HttpStatusCode.GatewayTimeout));
                 }
                 catch (Exception ex)
                 {
@@ -49,7 +70,6 @@ namespace Paradise.Service.Controller.Hotel.Reservas
                 }
             });
         }
-        // Reserva observaciones mucamas
         // Reserva historial
         // Reserva auditoria
     }
